678efd1560b81a59c022f7945ee018bb30447b23
Norbert Hartl <norbert@hartl.name>
2011-08-29 10:41:50 +0200

    added installation of the stone-creator utility. It uses git so a git cookbook is provided as well and added as base dependency

---
+++ vagrant/cookbooks/stone-creator/recipes/default.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: stone-creator
+# Recipe:: default
+#
+# Copyright 2011, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "gemstone"
+
+#execute "fetch stone-creator from gitorious" do
+#    command "git clone https://git.gitorious.org/gemstone-tools/stone-creator.git /opt/stone-creator"
+#    not_if { FileTest.exists?("/opt/stone-creator") }
+#end
+
+git "/opt/stone-creator" do
+   repository "https://git.gitorious.org/gemstone-tools/stone-creator.git"
+   reference "master"
+   action :sync
+end
+
+execute "creating application directory" do
+   command "mkdir /opt/application"
+   not_if { FileTest.exists?("/opt/application") }
+end
+
+execute "creating default stone instance" do
+   command "/opt/stone-creator/bin/create-stone.sh -n stone-default -d /opt/application/stone-default"
+   not_if { FileTest.exists?("/opt/application/stone-default") }
+end
+
+execute "creating symlink to /etc/init.d" do
+   command "ln -s /opt/application/stone-default/stone-default /etc/init.d/stone-default"
+   not_if { FileTest.exists?("/etc/init.d/stone-default") }
+end
+
+execute "activating stone-default" do
+   command "update-rc.d stone-default defaults"
+end
--- vagrant/cookbooks/virtual_gemstone/recipes/base.rb
+++ vagrant/cookbooks/virtual_gemstone/recipes/base.rb
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe "git"
 include_recipe "munin"
 include_recipe "nginx"
 include_recipe "gemstone"
-
-
+include_recipe "stone-creator"
