c6cc3c1a804babed42c589251027324285a5fd9f
Norbert Hartl <norbert@hartl.name>
2011-09-22 18:45:39 +0200

    Introduced system wide directory /etc/gemstone to be able to manage multiple stones. Added script to run mark for collection and backup

--- vagrant/cookbooks/stone-creator/recipes/default.rb
+++ vagrant/cookbooks/stone-creator/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "gemstone"
+include_recipe "gemstone::base"
 
 unless File.exists?("/opt/stone-creator")
    git "/opt/stone-creator" do
@@ -15,3 +15,40 @@ unless File.exists?("/opt/stone-creator")
       action :sync
    end
 end
+
+directory node[:stone_creator][:etc_dir] do
+   owner "#{node[:gemstone][:user]}"
+   mode "0755"
+   action :create
+end
+
+directory node[:stone_creator][:stones_dir] do
+   owner "#{node[:gemstone][:user]}"
+   mode "0755"
+   action :create
+end
+
+template "cleanup-stones" do
+  path "/opt/application/bin/cleanup-stones"
+  source "cleanup-stones.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+template "cleanup-stones cron" do
+  path "/etc/cron.d/cleanup-stones"
+  source "cleanup-cron.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+template "full-backup-stone" do
+  path "/opt/application/bin/full-backup-stone"
+  source "full-backup-stone.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
