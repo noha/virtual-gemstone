26ca6a23c09d535615ea1b07005be2361622e329
Norbert Hartl <norbert@hartl.name>
2011-09-23 16:59:57 +0200

    using GemStone library util to load code now. Cleans stuff from redundant stuff

--- vagrant/cookbooks/seaside/recipes/default.rb
+++ vagrant/cookbooks/seaside/recipes/default.rb
@@ -12,39 +12,27 @@ include_recipe "gemstone::netldi"
 script "install seaside" do
    interpreter "bash"
    user        "root"
-   code <<-EOH
-   source /opt/gemstone/product/seaside/defSeaside
-   echo "
-set gemstone stone-default
-set user DataCurator
-set password swordfish
-login
-run
-MCPlatformSupport autoCommit: true.
-MCPlatformSupport autoMigrate: true. 
-MCPlatformSupport commitOnAlmostOutOfMemoryDuring: [
-   ConfigurationOfGLASS project updateProject.
-   ConfigurationOfGLASS project latestVersion load ].
+   code GemStone.doIt("stone-default", "
+      MCPlatformSupport autoCommit: true.
+      MCPlatformSupport autoMigrate: true. 
+      MCPlatformSupport commitOnAlmostOutOfMemoryDuring: [
+         ConfigurationOfGLASS project updateProject.
+         ConfigurationOfGLASS project latestVersion load ].
 
-ConfigurationOfMetacello project updateProject.
-ConfigurationOfMetacello project latestVersion load.
+      ConfigurationOfMetacello project updateProject.
+      ConfigurationOfMetacello project latestVersion load.
 
-Gofer new
-   squeaksource: 'MetacelloRepository';
-   package: 'ConfigurationOfSeaside30';
-   load.
+      Gofer new
+         squeaksource: 'MetacelloRepository';
+         package: 'ConfigurationOfSeaside30';
+         load.
 
-MCPlatformSupport commitOnAlmostOutOfMemoryDuring: [
-   (Smalltalk at: #ConfigurationOfSeaside30) project lastVersion load
-].
+      MCPlatformSupport commitOnAlmostOutOfMemoryDuring: [
+         (Smalltalk at: #ConfigurationOfSeaside30) project lastVersion load
+      ].
 
-\\\"fix content length parsing problem. http://code.google.com/p/glassdb/issues/detail?id=298\\\"
-(Smalltalk at: #FSRole) compile: 'contentLengthHeader
-   ^((params at: ''CONTENT_LENGTH'' ifAbsent: [''0'']) ifEmpty: [''0'']) asNumber'      
-%
-commit
-logout
-exit" | topaz -l
-      EOH
-   not_if GemStone.isDefinedClass("stone-default", "WASession")
+      \\\"fix content length parsing problem. http://code.google.com/p/glassdb/issues/detail?id=298\\\"
+      (Smalltalk at: #FSRole) compile: 'contentLengthHeader
+         ^((params at: ''CONTENT_LENGTH'' ifAbsent: [''0'']) ifEmpty: [''0'']) asNumber'")
+   not_if GemStone.isDefinedClass("stone-default", "ConfigurationOfSeaside30")
 end
