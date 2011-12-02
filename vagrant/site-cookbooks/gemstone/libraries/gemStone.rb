069a4770a05bfecdc1f22c42214000736bb4c4f6
Norbert Hartl <norbert@hartl.name>
2011-12-02 11:56:45 +0100

    integrated dedicated GLASS update script into library so it is available to all other recipes

--- vagrant/site-cookbooks/gemstone/libraries/gemStone.rb
+++ vagrant/site-cookbooks/gemstone/libraries/gemStone.rb
@@ -36,4 +36,34 @@ exit 0\" | topaz -l"
    def self.doIt(stoneName, expression)
       prepareTopazTemplate(stoneName, expression)
    end
+
+   def self.updateGLASS(stoneName)
+      doIt(stoneName, "| versionString |
+versionString := '1.0-beta.8.7.1'.
+MCPlatformSupport
+  autoCommit: true;
+  autoMigrate: true.
+ConfigurationOfMetacello project currentVersion versionNumber < '1.0-beta.31.1' asMetacelloVersionNumber
+    ifTrue: [
+        (Gofer new)
+            gemsource: 'metacello';
+            version: 'Gofer-Core.gemstone-dkh.135';
+            version: 'Metacello-Base-DaleHenrichs.19';
+            version: 'Metacello-Core-dkh.468';
+            version: 'Metacello-MC-dkh.531';
+            version: 'Metacello-Platform.gemstone-dkh.23';
+            load].
+MCPlatformSupport commitOnAlmostOutOfMemoryDuring: [[[
+  ConfigurationOfGLASS project updateProject.
+  (ConfigurationOfGLASS project version: versionString) load: #( 'Core' 'Monticello' ).
+  (ConfigurationOfGLASS project version: versionString) load.
+ ]
+    on: (Smalltalk at: #MetacelloSkipDirtyPackageLoad)
+    do: [:ex | ex resume: false ]]
+        on: Warning
+        do: [:ex |
+            Transcript cr; show: ex description.
+            ex resume ]].
+      ")
+   end
 end
