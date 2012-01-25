51c7ccf3b9059e065e369e07befa5f992efe63a6
Willem van den Ende <willem.vandenende@gmail.com>
2012-01-25 13:55:34 +0100

    [nginx] #18 nginx install from source
    Extracted from a customer deployment. SSL should also be supported in this build,
    but is not switched on by default.
    
    Possible improvements:
    * place the nginx_installer directory in the new cache directory (so downloaded files remain between vagrant destroy and up, but is not as much in the way)
    * move the downloads from nginx_installer.sh, and parse the downloads for the version number; you download whatever.zip, but need to specify whatever345bc6 to make/configure. After a while this build breaks because the version numbers have moved on.
    * add ssl port to Vagrant (but conflicts easily with other apps, so maybe configurable).

---
+++ vagrant/site-cookbooks/nginx/recipes/source.rb
@@ -0,0 +1,70 @@
+#base recipe installs nginx from apt, and makes sure it is disabled and
+#stopped, so that we can copy an nginx compiled from source over the nginx binary from apt
+#while still benefitting from e.g. the start stop scripts and logging
+#location.
+#
+include_recipe "nginx::base"
+
+
+%w{build-essential libpcre3-dev libssl-dev  }.each do | name |
+  package name do
+    action :install
+  end
+end
+
+nginx_source_version = "1.0.1"
+nginx_build_dir = "/vagrant/nginx_installer/nginx-1.0.1/"
+nginx_installer_dir = "/vagrant/nginx_installer"
+
+directory "/vagrant/nginx_installer" do
+  owner "root"
+  group "root"
+  recursive true
+end
+
+cookbook_file "/vagrant/nginx_installer/nginx_installer.sh" do
+  source "nginx_installer.sh"
+  mode "0744"
+end
+
+#we use a script instead of remote_file because we need to rename the result and unzip it
+script "download upload progress" do
+  cwd nginx_installer_dir
+  interpreter "bash"
+  code <<-ENDOFSCRIPT
+wget -N --no-check-certificate https://github.com/masterzen/nginx-upload-progress-module/zipball/v0.8.2
+mv v0.8.2 nginx-upload-progress-module-v0.8.2.zip                                                             
+unzip nginx-upload-progress-module-v0.8.2.zip
+ENDOFSCRIPT
+   not_if {FileTest.exists?(IO::File.join(nginx_installer_dir,"nginx-upload-progress-module-v0.8.2.zip"))}
+end
+
+execute "compile nginx from source" do
+  cwd "/vagrant/nginx_installer"
+  command "bash ./nginx_installer.sh"
+  user "root"
+  not_if { FileTest.exists?(IO::File.join(nginx_build_dir, "objs/nginx")) }
+end
+        
+##http://www.nickager.com/blog/compiling-nginx-to-add-extra-modules?_s=_6ez9BdRc4-16Ktp&_k=Qk7hdWnSQQCfGsKw&_n&8
+execute "install nginx binary" do
+  cwd nginx_build_dir
+  command "cp objs/nginx /usr/sbin/nginx"
+  only_if  {FileTest.exists?("/vagrant/nginx_installer/objs/nginx")}
+end
+
+template "/etc/nginx/sites-enabled/default" do
+   source "server.conf.erb"
+   mode 644
+   owner "root"
+   group "root"
+   variables ({
+      :server_name => "localhost",
+      :application_path => "",
+      :upstream => $upstream})
+end
+
+service "nginx" do
+   supports :start => true, :restart => true, :reload => true
+   action [ :enable,  :start]
+end
